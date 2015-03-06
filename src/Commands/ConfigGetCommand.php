ad52dda25677a570a5920ae4692378e78df5a190
robinradic <robin@radic.nl>
2015-03-06 18:07:07 +0100

    made config get command

--- src/Commands/ConfigGetCommand.php
+++ src/Commands/ConfigGetCommand.php
@@ -5,6 +5,7 @@
 namespace Radic\Commands;
 
 
+use Symfony\Component\Console\Input\InputArgument;
 class ConfigGetCommand extends Command
 {
     protected $name = 'config:get';
@@ -12,6 +13,24 @@ class ConfigGetCommand extends Command
     protected $help = "Yo mama";
     public function fire()
     {
+        $args = $this->argument();
 
+        if ( ! isset($args['key']) )
+        {
+            $args['key'] = $this->ask('What key/path do you want to show? (leave empty if you want to show all)');
+        }
+        $val = radic()->config()->get($args['key']);
+
+        $this->info('Showing ' . $this->style(['bold', 'yellow'], $args['key']));
+        $this->dump($val);
+    }
+
+
+
+    protected function getArguments()
+    {
+        return array(
+            array('key', InputArgument::OPTIONAL, 'The key/path')
+        );
     }
 }
