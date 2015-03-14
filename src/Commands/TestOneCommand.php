fd41c0371773d9dd3501b5c473b3f0dd78f0ec70
robinradic <robin@radic.nl>
2015-03-14 12:19:10 +0100

    Tagging 1.0.6

---
+++ src/Commands/TestOneCommand.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Part of the Radic packages.
+ */
+namespace Radic\Commands;
+
+
+class TestOneCommand extends Command
+{
+
+    protected $name = 'test:one';
+
+    protected $description = 'Test 1';
+
+    protected $help = "Yo mama";
+
+    public function fire()
+    {
+        $this->dump([
+            'uid' => getmyuid(),
+            'gid' => getmygid(),
+            'pid' => getmypid(),
+            'dirname FILE' => dirname(__FILE__),
+            'cwd' => getcwd(),
+            'user' => get_current_user()
+        ]);
+    }
+}
