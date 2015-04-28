c9e8b3b0d9a69392a2e7b596d8ab30bc5fa82972
Robin Radic <robin@radic.nl>
2015-04-28 17:12:36 +0200

    fixed changelog generator

--- src/Commands/MakeChangelogCommand.php
+++ src/Commands/MakeChangelogCommand.php
@@ -16,10 +16,23 @@ class MakeChangelogCommand extends Command
     {
         $this->comment('Generating changelog..');
 
+
         if ( ! radic()->stubs->isExported() )
         {
             return $this->error('You have not exported the stubs file yet. Do so using make:export');
         }
+
+        // get owner/repo
+        $ini = parse_ini_string(radic()->fs->get(path_join(getcwd(), '.git/config')), true);
+        $segments = explode('/', $ini['remote origin']['url']);
+        $repo = last($segments);
+        array_pop($segments);
+        $owner = last($segments);
+        #return $this->dump(compact('ini', 'segments', 'repo', 'owner'));
+        $owner = $this->ask('Owner?', $owner);
+        $repo = $this->ask('Repository?', $repo);
+        $repoUrl = "https://github.com/$owner/$repo";
+
 
         exec('git log --date=short --format="- [%cd](commit/%H) %gs %s  " 2>&1', $commitLog, $return);
         preg_match_all('/-\s\[([\d-]*)\]\(commit\/([\w\d]*)\)[\s\t]*(.*)/', implode("\n", $commitLog), $matches);
@@ -37,10 +50,28 @@ class MakeChangelogCommand extends Command
             }
         }
 
-        radic()->stubs
-            ->to(getcwd())
-            ->generate([ 'test.stub' => false ]);
+        exec('git tag -l -n10 2>&1', $tagLog, $return);
 
-        exec('git tag -l -n10'); // 2>&1', $tagLog, $return);
+        $tags = [ ];
+        foreach ( $tagLog as $i => $tag )
+        {
+            preg_match('/^(\d)\.(\d)\.(\d)[\s\t]*(.*)/', $tag, $matches);
+            $tags[ ] = [
+                'version' => $matches[ 1 ] . '.' . $matches[ 2 ] . '.' . $matches[ 3 ],
+                'major'   => $matches[ 1 ],
+                'minor'   => $matches[ 2 ],
+                'patch'   => $matches[ 3 ],
+                'message' => $matches[ 4 ],
+            ];
+        }
+
+        $tags = array_reverse($tags);
+
+        radic()->stubs
+            ->set(compact('repoUrl', 'commits', 'tags'))
+            ->to(getcwd())
+            ->generate([ 'CHANGELOG.md.stub' => false ]);
+
+        $this->info('All done sire');
     }
 }
